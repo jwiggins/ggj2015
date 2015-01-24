0d2bcb1f228620c5ea3e43d29ba4fa774b94b7e7
John Wiggins <jwiggins@enthought.com>
2015-01-24 09:28:50 -0600

    Show the client's address on the server.

--- Server/Assets/StartServing.cs
+++ Server/Assets/StartServing.cs
@@ -4,6 +4,12 @@ using System.Collections;
 public class StartServing : MonoBehaviour {
 
     public int connectionPort = 25001;
+    public string connectedAddr;
+
+    void OnPlayerConnected(NetworkPlayer player)
+    {
+        connectedAddr = player.ipAddress;
+    }
 
     void OnGUI()
     {
@@ -20,7 +26,8 @@ public class StartServing : MonoBehaviour {
             string ipAddr = Network.player.ipAddress;
             GUI.Label(new Rect(10, 10, 300, 20), "Status: Connected as Server");
             GUI.Label(new Rect(10, 30, 300, 20), ipAddr);
-            if (GUI.Button(new Rect(10, 50, 120, 20), "Disconnect"))
+            GUI.Label(new Rect(10, 50, 300, 20), "client: " + connectedAddr);
+            if (GUI.Button(new Rect(10, 70, 120, 20), "Disconnect"))
             {
                 Network.Disconnect(200);
             }

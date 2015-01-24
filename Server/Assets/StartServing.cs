50f1300cb9e40e7811cb4896b470d84a74a9acab
John Wiggins <jwiggins@enthought.com>
2015-01-24 09:00:08 -0600

    Make the server more server-y

--- Server/Assets/StartServing.cs
+++ Server/Assets/StartServing.cs
@@ -2,36 +2,25 @@
 using System.Collections;
 
 public class StartServing : MonoBehaviour {
- 
-    public string connectionIP = "127.0.0.1";
+
     public int connectionPort = 25001;
- 
+
     void OnGUI()
     {
         if (Network.peerType == NetworkPeerType.Disconnected)
         {
             GUI.Label(new Rect(10, 10, 300, 20), "Status: Disconnected");
-            if (GUI.Button(new Rect(10, 30, 120, 20), "Client Connect"))
-            {
-                Network.Connect(connectionIP, connectionPort);
-            }
-            if (GUI.Button(new Rect(10, 50, 120, 20), "Initialize Server"))
+            if (GUI.Button(new Rect(10, 50, 120, 20), "Start Server"))
             {
                 Network.InitializeServer(32, connectionPort, false);
             }
         }
-        else if (Network.peerType == NetworkPeerType.Client)
-        {
-            GUI.Label(new Rect(10, 10, 300, 20), "Status: Connected as Client");
-            if (GUI.Button(new Rect(10, 30, 120, 20), "Disconnect"))
-            {
-                Network.Disconnect(200);
-            }
-        }
         else if (Network.peerType == NetworkPeerType.Server)
         {
+            string ipAddr = Network.player.ipAddress;
             GUI.Label(new Rect(10, 10, 300, 20), "Status: Connected as Server");
-            if (GUI.Button(new Rect(10, 30, 120, 20), "Disconnect"))
+            GUI.Label(new Rect(10, 30, 300, 20), ipAddr);
+            if (GUI.Button(new Rect(10, 50, 120, 20), "Disconnect"))
             {
                 Network.Disconnect(200);
             }

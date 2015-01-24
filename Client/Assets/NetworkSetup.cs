ac5de2c89cc605bf3fce75127c25d1d069625c1e
John Wiggins <jwiggins@enthought.com>
2015-01-24 08:16:49 -0600

    Add some stuff back to the client

---
+++ Client/Assets/NetworkSetup.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NetworkSetup : MonoBehaviour {
+ 
+    public string connectionIP;
+    public int connectionPort = 25001;
+ 
+    void OnGUI()
+    {
+        if (Network.peerType == NetworkPeerType.Disconnected)
+        {
+            GUI.Label(new Rect(10, 10, 300, 20), "Status: Disconnected");
+            connectionIP = GUI.TextField(new Rect(10, 30, 300, 20), connectionIP);
+            if (GUI.Button(new Rect(10, 50, 120, 20), "Client Connect"))
+            {
+                Network.Connect(connectionIP, connectionPort);
+            }
+        }
+        else if (Network.peerType == NetworkPeerType.Client)
+        {
+            GUI.Label(new Rect(10, 10, 300, 20), "Status: Connected as Client");
+            if (GUI.Button(new Rect(10, 30, 120, 20), "Disconnect"))
+            {
+                Network.Disconnect(200);
+            }
+        }
+    }
+}

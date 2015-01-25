a2f80c9d8f0d5b0b069bbaf82a339a0d082761f2
Tyler Hudson <tyler.hudson@g.austincc.edu>
2015-01-25 14:20:32 -0600

    Pre-final commit

---
+++ Server/Assets/Scripts/ForGreatJustice.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ForGreatJustice : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		StartCoroutine (waitAndJump ());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	IEnumerator waitAndJump () {
+		yield return new WaitForSeconds (5);
+		Application.LoadLevel ("OpeningScene");
+	}
+}

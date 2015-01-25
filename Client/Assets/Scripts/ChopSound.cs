b1841c0a8d90e90a55dece49faec84251e1ee2d3
John Wiggins <jwiggins@enthought.com>
2015-01-25 12:01:31 -0600

    Remove pauses from client attack scripts

--- Client/Assets/Scripts/ChopSound.cs
+++ Client/Assets/Scripts/ChopSound.cs
@@ -5,7 +5,6 @@ public class ChopSound : Sound
 {
 	protected override void Awake()
 	{
-		pause = 1.0f;
 		base.Awake();
 	}
 }
--- Client/Assets/Scripts/SlowSound.cs
+++ Client/Assets/Scripts/SlowSound.cs
@@ -5,7 +5,6 @@ public class SlowSound : Sound
 {
 	protected override void Awake()
 	{
-		pause = 5.0f;
 		base.Awake();
 	}
 }

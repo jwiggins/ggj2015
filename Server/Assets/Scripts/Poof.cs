44c3733e1b9a081dfddcd20ccc9e616600906a3a
Tyler Hudson <tyler.hudson@g.austincc.edu>
2015-01-25 14:02:39 -0600

    GUI update

---
+++ Server/Assets/Scripts/Poof.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Poof : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		StartCoroutine (die ());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	IEnumerator die () {
+		yield return new WaitForSeconds (1);
+		Destroy (gameObject);
+	}
+}
